e2d8dd2b94f62c41a08fe78d18fc4f35f5f64765
himaloyash <himaloyashis98@gmail.com>
2023-04-18 13:23:47 +0600

    problem

--- src/shared/LeftNav/LeftNav.jsx
+++ src/shared/LeftNav/LeftNav.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+    // const [data, setData ] = useState([]) 
+
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/category')
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+        .catch(error=>console.log(error.message))
+    },[])
+
     return (
         <div>
             <h2>leftNav</h2>
